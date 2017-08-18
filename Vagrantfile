249cf40f6ade2a2effdee425642ac3c8657343ed
Andrey Galkin <andrey@futoin.org>
2017-08-18 21:30:18 +0300

    Prepare for MySQL service testing

---
+++ Vagrantfile
@@ -0,0 +1,22 @@
+
+Vagrant.configure("2") do |config|
+    config.vm.define 'db' do |node|
+        node.vm.provider "virtualbox" do |v|
+            v.memory = 512
+        end
+        node.vm.box = "bento/ubuntu-16.04"
+
+        node.vm.network "forwarded_port", guest: 3306, host: 3306, host_ip: "127.0.0.1"
+        node.vm.network "forwarded_port", guest: 5432, host: 5432, host_ip: "127.0.0.1"
+
+        node.vm.provision "shell", inline: <<-SHELL
+            apt-get update
+            DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server postgresql
+            sed -i -e s/127.0.0.1/0.0.0.0/g /etc/mysql/mysql.conf.d/mysqld.cnf
+            echo "listen_addresses = '*'" >> /etc/postgresql/9.5/main/postgresql.conf
+            echo "host all all 0/0 md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+            systemctl restart mysql.service postgresql.service
+            mysql -e "CREATE USER 'root'@'%'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'"
+        SHELL
+    end
+end
--- test/unittest.js
+++ test/unittest.js
@@ -882,28 +882,6 @@ describe('XferBuilder', function() {
     });
 });
 
-describe('MySQLDriver', function() {
-    const drv = QueryBuilder.getDriver('mysql');
-
-    it('should escape values correctly', () => {
-        expect( drv.escape(true) ).to.equal('true');
-        expect( drv.escape(false) ).to.equal('false');
-        expect( drv.escape(0) ).to.equal('0');
-        expect( drv.escape(100) ).to.equal('100');
-        expect( drv.escape(-300) ).to.equal('-300');
-        expect( drv.escape(1.5) ).to.equal('1.5');
-        expect( drv.escape("") ).to.equal("''");
-        expect( drv.escape("Some ' string ' \" \\") )
-            .to.equal("'Some \\' string \\' \\\" \\\\'");
-    });
-    
-    it('should escape identifiers correctly', () => {
-        expect( drv.identifier('one') ).to.equal('`one`');
-        expect( drv.identifier('one.two') ).to.equal('`one`.`two`');
-        expect( drv.identifier('on`e.t`w`o') ).to.equal('`on``e`.`t``w``o`');
-    });
-});
-
 
 describe('PostgreSQLDriver', function() {
     const drv = QueryBuilder.getDriver('postgresql');
