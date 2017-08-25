2421436303b7fa09705350eefca52097a2ecf3d4
Andrey Galkin <andrey@futoin.org>
2017-08-25 17:02:29 +0300

    Changed not to do custom DB install on Travis

--- test/db_config.sh
+++ test/db_config.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-apt-get update
+if test -z "$TRAVIS"; then
+    apt-get update
 
-# Install
-DEBIAN_FRONTEND=noninteractive apt-get install -y \
-    mysql-server \
-    postgresql
+    # Install
+    DEBIAN_FRONTEND=noninteractive apt-get install -y \
+        mysql-server \
+        postgresql
 
-# Listen on all interfaces
-sed -i -e s/127.0.0.1/0.0.0.0/g /etc/mysql/mysql.conf.d/mysqld.cnf
-#
-echo "listen_addresses = '*'" >> /etc/postgresql/9.5/main/postgresql.conf
-echo "host all all 0/0 md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+    # Listen on all interfaces
+    sed -i -e s/127.0.0.1/0.0.0.0/g /etc/mysql/mysql.conf.d/mysqld.cnf
+    #
+    echo "listen_addresses = '*'" >> /etc/postgresql/9.5/main/postgresql.conf
+    echo "host all all 0/0 md5" >> /etc/postgresql/9.5/main/pg_hba.conf
 
-# Restart services
-systemctl restart mysql.service postgresql.service
+    # Restart services
+    systemctl restart mysql.service postgresql.service
+fi
 
 # Add test users
 mysql -e "CREATE USER 'ftntest'@'%'; GRANT ALL PRIVILEGES ON *.* TO 'ftntest'@'%'"
